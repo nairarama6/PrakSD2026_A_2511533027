1fd19166ceddc8a658083f9dfda8249798027330
hp <hp@LAPTOP-MGK4V63A>
2026-04-02 22:14:59 +0700

    tugas praktikum pekan 1

--- PrakSD2026_A_2511533027/src/pekan1_2511533027/ADTMobil_2511533027.java
+++ PrakSD2026_A_2511533027/src/pekan1_2511533027/ADTMobil_2511533027.java
@@ -1,5 +1,49 @@
 package pekan1_2511533027;
 
 public class ADTMobil_2511533027 {
+	// atribut
+    String nama;
+    int tahun;
+    int cc;
+    long harga;
+    String merk;
 
+    // konstruktor
+    public ADTMobil_2511533027(String nama, int tahun, int cc, long harga, String merk) {
+        this.nama = nama;
+        this.tahun = tahun;
+        this.cc = cc;
+        this.harga = harga;
+        this.merk = merk;
+    }
+    
+ // selektor (getter)
+    public String getNama() {
+        return nama;
+    }
+
+    public int getTahun() {
+        return tahun;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public long getHarga() {
+        return harga;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    // method tampil (opsional, biar gampang)
+    public void tampil() {
+        System.out.println("Nama Mobil: " + nama);
+        System.out.println("Tahun: " + tahun);
+        System.out.println("cc: " + cc);
+        System.out.println("Harga: " + harga);
+        System.out.println("Merk: " + merk);
+    }
 }
--- PrakSD2026_A_2511533027/src/pekan1_2511533027/Driver_2511533027.java
+++ PrakSD2026_A_2511533027/src/pekan1_2511533027/Driver_2511533027.java
@@ -1,5 +1,48 @@
 package pekan1_2511533027;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Driver_2511533027 {
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<ADTMobil_2511533027> list = new ArrayList<>();
+
+        System.out.print("Masukkan jumlah mobil: ");
+        int n = input.nextInt();
+        input.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nData Mobil ke-" + (i + 1));
+
+            System.out.print("Nama Mobil: ");
+            String nama = input.nextLine();
+
+            System.out.print("Tahun: ");
+            int tahun = input.nextInt();
+
+            System.out.print("cc: ");
+            int cc = input.nextInt();
+
+            System.out.print("Harga: ");
+            long harga = input.nextLong();
+            input.nextLine();
+
+            System.out.print("Merk: ");
+            String merk = input.nextLine();
+
+            // buat objek mobil
+            ADTMobil_2511533027 m = new ADTMobil_2511533027(nama, tahun, cc, harga, merk);
+            // tambah ke ArrayList
+            list.add(m);
+        }
+
+        // OUTPUT
+        System.out.println("\n=== DATA MOBIL ===");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("\nData Mobil ke-" + (i + 1));
+            list.get(i).tampil();
+        }
+    }
 
 }
